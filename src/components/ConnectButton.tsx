cf8db034d784d726325e3e911d1e018dba73ae31
MaxBase40 <basefortyblock@gmail.com>
2026-02-05 10:38:39 +0700

    Refactor ConnectButton to use OnchainKitProvider
    Refactored ConnectButton to Providers component and integrated OnchainKitProvider.

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,29 +1,26 @@
-// src/components/ConnectButton.tsx
 'use client';
+import { OnchainKitProvider } from '@coinbase/onchainkit';
+import { base } from 'viem/chains';
 
-import {
-  ConnectWallet,
-  Wallet,
-  WalletDropdown,
-  WalletDropdownDisconnect,
-} from '@coinbase/onchainkit/wallet';
-import { Avatar, Name, Address, Identity } from '@coinbase/onchainkit/identity';
-
-export function ConnectButton() {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <Wallet>
-      <ConnectWallet>
-        <Avatar className="h-6 w-6" />
-        <Name />
-      </ConnectWallet>
-      <WalletDropdown>
-        <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
-          <Avatar />
-          <Name />
-          <Address />
-        </Identity>
-        <WalletDropdownDisconnect />
-      </WalletDropdown>
-    </Wallet>
+    <OnchainKitProvider
+      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+      chain={base}
+      config={{
+        appearance: {
+          name: 'Baseforty',
+          mode: 'auto',
+          theme: 'default',
+        },
+        wallet: {
+          display: 'modal', // Tambahkan ini!
+          termsUrl: 'https://...',
+          privacyUrl: 'https://...',
+        },
+      }}
+    >
+      {children}
+    </OnchainKitProvider>
   );
 }
